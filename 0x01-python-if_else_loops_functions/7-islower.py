28a588cd338899bec983efb57be5eac81e33a123
djso89 <danielso89@gmail.com>
2020-04-28 17:47:18 -0700

    changed return statements

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return 1
+    if ord(c) > 96 and ord(c) < 122:
+        return True
     else:
-        return 0
+        return False
