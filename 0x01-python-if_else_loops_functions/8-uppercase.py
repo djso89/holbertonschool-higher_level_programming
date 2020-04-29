8e34461c7cc7498c6b535f423bb9dd0b342684e4
djso89 <danielso89@gmail.com>
2020-04-28 17:54:23 -0700

    first commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for s in str:
+        if ord(s) > 96 and ord(s) < 123:
+            print("{:c}".format(ord(s) - 32), end="")
+        else:
+            print("{:c}".format(ord(s)), end="")
+    print("")
