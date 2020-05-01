f2eca74f078f31cad4facec523f847fbe7603ef1
djso89 <danielso89@gmail.com>
2020-04-30 17:42:45 -0700

    wrong file name

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+
+    ac = len(sys.argv)
+    if ac == 2:
+        print("1 argument:")
+        print("1: {:s}".format(sys.argv[1]))
+    elif ac > 2:
+        print("{:d} arguments".format(ac - 1))
+        for i in range(ac - 1):
+            print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
+    else:
+        print("0 arguments")
