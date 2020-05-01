03c1ab0158c216b5cd102d6b2ed1235333f24940
djso89 <danielso89@gmail.com>
2020-04-30 17:47:35 -0700

    forgot the colon

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
         print("1 argument:")
         print("1: {:s}".format(sys.argv[1]))
     elif ac > 2:
-        print("{:d} arguments".format(ac - 1))
+        print("{:d} arguments:".format(ac - 1))
         for i in range(ac - 1):
             print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
     else:
-        print("0 arguments")
+        print("0 arguments.")
