f0eb71a256a53bf221dd0a212574fd344686641b
djso89 <danielso89@gmail.com>
2020-05-05 16:27:01 -0700

    added if else

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
-    l_f = length, first
-    return l_f
+    if sentence:
+        length = len(sentence)
+        first = sentence[0]
+        l_f = length, first
+        return l_f
+    else:
+        return 0, "None"
