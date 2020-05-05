1331e6b72714d6264989d02c7a50db05cc918603
djso89 <danielso89@gmail.com>
2020-05-05 15:38:54 -0700

    fixed the printing format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
+    if matrix is None:
         print()
+    else:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if j is not len(matrix) - 1:
+                    print("{:d}".format(matrix[i][j]), end=" ")
+                else:
+                    print("{:d}".format(matrix[i][j]), end="")
+            print()
