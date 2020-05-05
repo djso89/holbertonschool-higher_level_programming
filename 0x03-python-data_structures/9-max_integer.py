cf1a85bd9ede25274105c5f6a7f0b6f8f9593acb
djso89 <danielso89@gmail.com>
2020-05-05 16:33:46 -0700

    first commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    else:
+        c = my_list[0]
+        for i in my_list:
+            if i > c:
+                c = i
+        return c
