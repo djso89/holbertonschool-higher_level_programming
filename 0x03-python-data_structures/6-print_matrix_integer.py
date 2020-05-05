a6512981540315c0a3380261419accb8d5933f7c
djso89 <danielso89@gmail.com>
2020-05-05 15:48:24 -0700

    forgot inner square bracket

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[]):
+def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
