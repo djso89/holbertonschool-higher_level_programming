db460e56ce0a8de5a940d947a9ea431ecce9bf69
djso89 <danielso89@gmail.com>
2020-05-07 21:11:36 -0700

    modified if

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+    roman_string = roman_string.upper()
     value = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     prev = 0
     ans = 0
