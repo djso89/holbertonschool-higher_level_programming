8aabf1cbc3fe51523fa097495c7348bb61b32a54
djso89 <danielso89@gmail.com>
2020-05-19 16:31:48 -0700

    reduced comments

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,12 +6,8 @@ class Square:
     '''initializing private attribute'''
     def __init__(self, size=0):
         '''assign the size'''
-        self.__size = size
-        '''check if the size is integer'''
         if not isinstance(size, int):
-            '''raise the Type Error'''
             raise TypeError("size must be an integer")
-        ''' check if size is negative number'''
         elif size < 0:
-            '''raise value error '''
             raise ValueError("size must be >= 0")
+        self.__size = size
