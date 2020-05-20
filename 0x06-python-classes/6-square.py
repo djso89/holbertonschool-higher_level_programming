60c3675d0c2652b6a4a55bc0e4b23f4b82ce0359
djso89 <danielso89@gmail.com>
2020-05-19 23:02:30 -0700

    changed if statement in my_print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,7 +15,7 @@ class Square():
 
     def my_print(self):
         ''' print out the square in #'''
-        if self.__size <= 0:
+        if self.__size == 0:
             print()
         else:
             for i in range(self.__position[1]):
