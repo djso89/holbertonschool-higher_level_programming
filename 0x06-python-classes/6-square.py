75ce14168418e44bee79d06a7cd7c2c167e4e1d5
djso89 <danielso89@gmail.com>
2020-05-19 23:06:44 -0700

    changed init

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square():
     '''create square object'''
     def __init__(self, size=0, position=(0, 0)):
         ''' initialize size'''
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         '''calculate the area of square '''
