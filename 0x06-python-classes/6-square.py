a2be9b85d334b056b17addf7e04adcaa5c1f32a8
djso89 <danielso89@gmail.com>
2020-05-19 22:58:03 -0700

    added position feature

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''Define class Square '''
+
+
+class Square():
+    '''create square object'''
+    def __init__(self, size=0, position=(0, 0)):
+        ''' initialize size'''
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        '''calculate the area of square '''
+        return self.__size * self.__size
+
+    def my_print(self):
+        ''' print out the square in #'''
+        if self.__size <= 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
+
+    @property
+    def size(self):
+        ''' retrieve the size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        ''' set the value to size '''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        ''' position of the square'''
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("postition must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
