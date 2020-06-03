cbfd87f13e37a57740eff604accfc01c5406d47b
djso89 <danielso89@gmail.com>
2020-06-02 22:56:40 -0700

    fixed doc format

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+BaseGeometry module
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
+"""Class Rectangle """
 class Rectangle(BaseGeometry):
     """Rectangle class """
     def __init__(self, width, height):
