dfc88d7120ddf679c9a7dfe78a42f26c87150c51
djso89 <danielso89@gmail.com>
2020-06-02 23:17:47 -0700

    first commit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Rectangle module
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+"""Class Square """
+
+
+class Square(Rectangle):
+    """Square class """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """return the area """
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
