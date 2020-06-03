39a8c128076f2d7c112eb833c697e1cee46b793e
djso89 <danielso89@gmail.com>
2020-06-03 16:52:29 -0700

    fixed doc format

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+""" number of lines module """
+
+
 def number_of_lines(filename=""):
+    """
+    function that returns
+    the number of lines of a text file
+    """
     line_num = 0
     with open(filename) as f:
         for line in f:
