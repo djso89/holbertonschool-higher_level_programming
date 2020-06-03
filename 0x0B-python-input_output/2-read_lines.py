ad09d41a37e8e4dece59ee2ddbac9fb98e79d623
djso89 <danielso89@gmail.com>
2020-06-03 16:46:48 -0700

    first commit

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    """function that reads n lines of a text file
+    and prints it to stdout"""
+    f = open(filename)
+    cnt = len(f.readlines())
+    with open(filename) as contents:
+        if nb_lines <= 0 or nb_lines > cnt:
+            words = contents.read()
+            print(words, end='')
+        else:
+            for line in range(nb_lines):
+                line = contents.readline()
+                print(line, end='')
