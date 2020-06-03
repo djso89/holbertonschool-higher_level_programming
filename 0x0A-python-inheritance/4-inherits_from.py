293d1d3ff95de8169d55b81cc907c6059a49772c
djso89 <danielso89@gmail.com>
2020-06-02 20:53:36 -0700

    added comment

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,6 +3,9 @@
 
 
 def inherits_from(obj, a_class):
+    """check if object is instance of class
+    that inherited from the specified class
+    """
     if type(obj) == a_class:
         return False
     return isinstance(obj, a_class)
