386daf52fc2eb9e86549c6b2db206564a63b0c5a
djso89 <danielso89@gmail.com>
2020-06-02 22:04:29 -0700

    first commit

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""initializing empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate the value of integer"""
+        if not isinstance(value, int):
+            raise TypeError(name + "must be an integer")
+        if value < 1:
+            raise ValueError(name + "must be greater than 0")
