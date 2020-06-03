2855ad0e86563b139f4d7e9dca41b560be66b8d1
djso89 <danielso89@gmail.com>
2020-06-02 19:08:17 -0700

    following doc format

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Lookup module"""
+
+
 def lookup(obj):
-    """function that returns the list of available attributes
-    and methods of an object"""
+    """
+    function that returns the list of available attributes
+    and methods of an object
+    """
     if obj:
         return dir(obj)
     else:
