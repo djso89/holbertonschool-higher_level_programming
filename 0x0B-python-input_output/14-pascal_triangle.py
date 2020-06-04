d4503e9b8edb12be4daf5615927159e52e00cef1
djso89 <danielso89@gmail.com>
2020-06-03 23:39:14 -0700

    submit

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""pascal_triangle module """
+
+
+def pascal_triangle(n):
+    pt_list = []
+
+    if n <= 0:
+        return pt_list
+    else:
+        for i in range(n):
+            new_list = []
+            new_list.append(1)
+            if len(pt_list) > 1:
+                for k in range(len(pt_list[(len(pt_list) -1)])):
+                    if k < (len(pt_list[(len(pt_list) - 1)]) - 1):
+                        new_list.append(pt_list[len(pt_list) - 1][k] +
+                                        pt_list[len(pt_list) - 1][k + 1])
+            if len(pt_list) > 0:
+                new_list.append(1)
+            pt_list.append(list(new_list))
+        return pt_list
