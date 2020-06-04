01e671f76dbffde9a3053a8bb691aa7ea2034c11
djso89 <danielso89@gmail.com>
2020-06-03 17:09:24 -0700

    fixed doc format

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -3,6 +3,11 @@
 
 
 def write_file(filename="", text=""):
+    """
+    function that writes string to a text
+    file and returns the number of characters
+    written
+    """
     with open(filename, 'w') as f:
         chars = f.write(text)
     return chars
