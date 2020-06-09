e824c20968475fc8211f91d28b27dabcc91fd40b
djso89 <danielso89@gmail.com>
2020-06-09 15:34:59 -0700

    added __str__

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,3 +11,8 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """initialize Square instances """
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """overloading __str__ method for square """
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.height)
