5a4b47b2051dc43f2a2cae3d0cd00e58033e1ce7
djso89 <danielso89@gmail.com>
2020-06-09 15:01:34 -0700

    task6: __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,8 @@ class Rectangle(Base):
         """Prints in stdout the rectangle """
         for i in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        """updated str method """
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format
+                (self.id, self.__x, self.__y, self.__width, self.__height))
