916270122cbee59ddd24b7f4a8316e0c717ea06d
djso89 <danielso89@gmail.com>
2020-06-09 15:26:55 -0700

    Task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Square module for square class
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Square """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize Square instances """
+        super().__init__(size, size, x, y, id)
