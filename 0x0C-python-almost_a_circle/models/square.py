ca29b3bed9c965b0f1c90b4189ef5e32d2a09552
djso89 <danielso89@gmail.com>
2020-06-09 15:50:11 -0700

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,16 @@ Square module for square class
 from models.rectangle import Rectangle
 
 
+def integer_validator(name, value):
+    """ Validate value """
+    if type(value) is not int:
+        raise TypeError('{} must be an integer'.format(name))
+    if value <= 0 and name in ['width', 'height']:
+        raise ValueError('{} must be > 0'.format(name))
+    if value < 0 and name in ['x', 'y']:
+        raise ValueError('{} must be >= 0'.format(name))
+
+
 class Square(Rectangle):
     """class Square """
     def __init__(self, size, x=0, y=0, id=None):
@@ -16,3 +26,15 @@ class Square(Rectangle):
         """overloading __str__ method for square """
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.height)
+
+    @property
+    def size(self):
+        """get the size of square """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set size """
+        integer_validator('width', value)
+        self.height = value
+        self.width = value
