50e3866e855db0f9e807f8e763637a609aa99716
djso89 <danielso89@gmail.com>
2020-06-09 13:40:24 -0700

    typo def

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,54 +3,56 @@
 Rectangle Module
 """
 
+
 from models.base import Base
 
 
-def Rectangle(base):
+class Rectangle(Base):
     """ Rectanggle Class """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
-        self.y =y
+        self.y = y
 
     @property
     def width(self):
         """get the width """
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """set the width """
-        self.width = value
+        self.__width = value
 
     @property
     def height(self):
         """get the height """
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """set the height """
-        self.height = value
+        self.__height = value
 
     @property
     def x(self):
         """get the x """
-        return self.x
+        return self.__x
 
     @x.setter
     def x(self, value):
         """set the x """
-        self.x = value
+        self.__x = value
 
     @property
     def y(self):
         """get the y """
-        return self.y
+        return self.__y
 
     @x.setter
     def y(self, value):
         """set the y """
-        self.y = value
+        self.__y = value
