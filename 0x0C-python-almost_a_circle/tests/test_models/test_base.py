e50ddaffe8f0d136d3b4c9693afda794a798883b
djso89 <danielso89@gmail.com>
2020-06-09 18:46:38 -0700

    first commit

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Unittesting for Class - Base
+"""
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+class BaseTest(unittest.TestCase):
+    """clas Base unittest """
+    def test_increment(self):
+        """test to see if id increments """
+        Base.__Base__nb_objects = 0
+
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
