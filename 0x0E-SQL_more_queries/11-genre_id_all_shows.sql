41f7df915c7bb0c79c5b9ffe4e5035c6409b2ff5
djso89 <danielso89@gmail.com>
2020-07-14 23:04:44 -0700

    LEFT OUTER and added ASC at the end

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,4 @@
 -- script that lists all shows contained in the database hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre.id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY title, genre_id;
+SELECT tv_shows.title, tv_show_genres.genre.id FROM tv_shows
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title, genre_id ASC;
