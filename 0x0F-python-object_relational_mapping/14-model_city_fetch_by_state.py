8b4d591b962318eafd5028393f1eb858cb9d760a
djso89 <danielso89@gmail.com>
2020-08-10 00:15:37 -0700

    fixed pep8

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -21,7 +21,8 @@ if __name__ == "__main__":
     session.configure(bind=engine)
     session__ = session()
 
-    res = session__.query(State, City).filter(City.state_id == State.id).\
+    res =
+    session__.query(State, City).filter(City.state_id == State.id).
     order_by(City.id).all()
 
     for get in res:
