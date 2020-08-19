e6fd78ebb0da10128a48bf1ea839bdc9e2f8e3c6
djso89 <danielso89@gmail.com>
2020-08-18 22:08:38 -0700

    fixed header

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #  a Bash script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
 curl -ILs "$1" | grep "Content-Length: " | awk '{print $2}'
