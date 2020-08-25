e248c76d99d46120a642bb66ec83e384b8bead6a
djso89 <danielso89@gmail.com>
2020-08-25 01:08:28 -0700

    task 10: my git hub

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a Python script that takes your Github credentials
+(username and password) and uses the Github API to display your id
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    values = (argv[1], argv[2])
+
+    r = requests.get('https://api.github.com/user', auth=values)
+    j_dict = r.json()
+    print(j_dict.get('id'))
