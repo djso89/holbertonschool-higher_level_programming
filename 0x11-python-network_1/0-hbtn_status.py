73964e4a37963fdc30962cc9359c7cfadc7686b0
djso89 <danielso89@gmail.com>
2020-08-24 23:54:52 -0700

    fixed some typos

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,5 +9,6 @@ import urllib.request as httpreq
 if __name__ == "__main__":
     with httpreq.urlopen(" https://intranet.hbtn.io/status") as spot:
         spot_read = spot.read()
-        print("Body Response:\n\t- type: {}\n\t- content: {}\n\t
-        - utf8 content: {}".format(type(spot_read), spot_read, spot_read.decode("utf-8")))
+        print("Body Response:\n\t- type: {}\n\t- content: {}\n\t\
+- utf8 content: {}".
+              format(type(spot_read), spot_read, spot_read.decode("utf-8")))
