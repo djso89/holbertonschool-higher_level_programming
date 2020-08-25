38f4f4011a15e3d85667e4471d9bfb2ebf308d87
djso89 <danielso89@gmail.com>
2020-08-24 23:49:39 -0700

    task 0 what is my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ a Python script that fetches https://intranet.hbtn.io/status
+"""
+
+
+import urllib.request as httpreq
+
+if __name__ == "__main__":
+    with httpreq.urlopen(" https://intranet.hbtn.io/status") as spot:
+        spot_read = spot.read()
+        print("Body Response:\n\t- type: {}\n\t- content: {}\n\t
+        - utf8 content: {}".format(type(spot_read), spot_read, spot_read.decode("utf-8")))
