b4b974fbfd00a18a3c8505313e9b36304c4c26ee
djso89 <danielso89@gmail.com>
2020-08-25 00:27:10 -0700

    4. What's my status? #1' '

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches https://intranet.hbtn.io/status
+"""
+
+if __name__ == "__main__":
+    import requests
+
+    r = request.get("https://intranet.hbtn.io/status")
+
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
