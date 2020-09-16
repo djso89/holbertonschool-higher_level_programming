3c9c54e30601c646aef9e191c7415f20ca0a2c8a
djso89 <danielso89@gmail.com>
2020-09-16 00:40:33 -0700

    task 10:factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function factorial (number) {
+  if (isNaN(number) || number == (1 || 0)) {
+    return 1;
+  }
+
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(Number(process.argv[2])));
