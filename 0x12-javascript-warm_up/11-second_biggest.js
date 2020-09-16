2dc8a46c5fee86a5f60fd16cb89a50e983901687
djso89 <danielso89@gmail.com>
2020-09-16 01:11:58 -0700

    fixed logic

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,10 @@
 #!/usr/bin/node
 
-function secondbig (arr) {
-  if (arr.length === 1 || arr.length === 0) return 0;
-  let top = -Infinity;
-  let top1 = -Infinity;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > top) {
-      top1 = top;
-      top = arr[i];
-    } else if (top > arr[i] > top1) {
-      top1 = arr[i];
-    }
-  }
-  return top1;
+const input = process.argv.length - 2;
+if (input < 2) {
+  console.log(0);
+} else {
+  const array = process.argv.slice(2);
+  const sorted = array.sort();
+  console.log(array[sorted.length - 2]);
 }
-
-const arr = process.argv.slice(2, process.argv.length).map(Number);
-console.log(secondbig(arr));
