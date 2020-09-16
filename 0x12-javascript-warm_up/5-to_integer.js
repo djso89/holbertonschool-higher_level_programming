61e659ac01e9b286cfcdddf1982609e2025ea6ec
djso89 <danielso89@gmail.com>
2020-09-16 00:26:10 -0700

    fixed logic

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-if (Number.isInteger(process.argv[2])) {
-  console.log('Not a number');
+let number;
+if (process.argv.length > 2) {
+  number = parseInt(process.argv[2]);
+
+  if (!isNaN(number)) {
+    console.log('My number: ' + process.argv[2]);
+  } else {
+    console.log('Not a number');
+  }
 } else {
-  console.log('My number: ' + process.argv[2]);
+  console.log('Not a number');
 }
