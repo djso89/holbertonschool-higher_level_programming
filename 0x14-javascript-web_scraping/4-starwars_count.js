3d67f31e3af204129dfa4d47939014669c0b9aa1
djso89 <danielso89@gmail.com>
2020-09-22 23:53:29 -0700

    task 4: star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let cnt = 0;
+    const films = JSON.parse(body).results;
+    for (let i = 0; i < films.length; i++) {
+      for (let j = 0; j < films[i].characters.length; j++) {
+        if (films[i].characters[j].includes('/18/')) {
+          cnt++;
+          break;
+        }
+      }
+    }
+    console.log(cnt);
+  }
+});
